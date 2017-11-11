a3bf4dcb08fdf51102999a66ddacac3b03a93919
GWigWam <welwisam@gmail.com>
2017-11-11 19:50:26 +0100

    Add build / run scripts

---
+++ build/Compile.ps1
@@ -0,0 +1,31 @@
+Set-PSDebug -Trace 1
+
+$mainclass_path = '../src/pack/Main.java'
+
+$build_outp = 'bin'
+$lwjgl_dir = '../lwjgl-2.8.5'
+$lwjgl_dll_dir = $lwjgl_dir + '/native/windows'
+$lwjgl_jar = $lwjgl_dir + '/jar/lwjgl.jar'
+
+$res = '../files'
+
+$jar_outp = 'jar'
+$jar_name = 'WormsEvo.jar'
+$jar_manifest = 'MANIFEST.MF'
+
+If(!(Test-Path $build_outp)) { New-Item -ItemType Directory -Path $build_outp }
+If(!(Test-Path $jar_outp)) { New-Item -ItemType Directory -Path $jar_outp }
+
+echo 'Compiling...'
+javac -cp ($lwjgl_jar + ";" + $slick_jar + ";../src/") $mainclass_path -d $build_outp
+echo 'Compiling done'
+
+Copy-Item $lwjgl_jar $jar_outp
+Copy-Item ($lwjgl_dll_dir + '/lwjgl.dll') $jar_outp
+Copy-Item ($lwjgl_dll_dir + '/lwjgl64.dll') $jar_outp
+
+echo 'Creating executable jar...'
+jar -cfme ($jar_outp + '\' + $jar_name) $jar_manifest pack.Main -C $build_outp pack
+echo 'Jar creation done'
+
+pause
---
+++ build/Run.ps1
@@ -0,0 +1,3 @@
+cd jar
+java -jar WormsEvo.jar
+pause
